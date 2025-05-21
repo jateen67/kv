83b92047f55fd5d412d86a406c2134ea8a91bd53
exit <kalsijatin67@gmail.com>
2025-05-21 02:08:04 -0400

    refactor store methods + stub close()

--- internal/store.go
+++ internal/store.go
@@ -1,7 +1,7 @@
 package internal
 
 type Store interface {
-	Get(key string) string
-	Set(key string, value string)
+	Get(key string) (string, error)
+	Set(key string, value string) error
 	Close() bool
 }
