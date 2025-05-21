c378aff29293024898878fab049da72b0a79df23
exit <kalsijatin67@gmail.com>
2025-05-21 14:07:02 -0400

    http layer for client requests

--- http/service.go
+++ http/service.go
@@ -1,24 +1,24 @@
 package http
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net"
 	"net/http"
-)
+	"strings"
 
-type Store interface {
-	Put(key string, value string) error
-	Get(key string) (string, error)
-	Close() bool
-}
+	"github.com/jateen67/kv/internal"
+)
 
 type Service struct {
 	addr  string
 	ln    net.Listener
-	store Store
+	store internal.Store
 }
 
 // return unitialized HTTP service
-func NewService(addr string, store Store) *Service {
+func NewService(addr string, store internal.Store) *Service {
 	return &Service{
 		addr:  addr,
 		store: store,
@@ -54,11 +54,66 @@ func (s *Service) Close() error {
 }
 
 func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	panic("implement this")
+	if strings.HasPrefix(r.URL.Path, "/key") {
+		s.handleKeyRequest(w, r)
+	} else {
+		fmt.Println("does not have /key prefix")
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func (s *Service) handleKeyRequest(w http.ResponseWriter, r *http.Request) {
+	getKey := func() string {
+		parts := strings.Split(r.URL.Path, "/")
+		if len(parts) != 3 {
+			return ""
+		}
+		return parts[2]
+	}
 
+	switch r.Method {
+	case "POST":
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		m := map[string]string{}
+		if err := json.Unmarshal(b, &m); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+
+		for k, v := range m {
+			if err := s.store.Set(&k, &v); err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+		}
+
+	case "GET":
+		k := getKey()
+		if k == "" {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		val, err := s.store.Get(k)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		io.WriteString(w, val)
+
+	case "DELETE":
+		k := getKey()
+		if k == "" {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		err := s.store.Delete(k)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
 }
 
 func (s *Service) Addr() net.Addr {
